48c05ac9823bece322fc4a47736fffe49a3608bf
Noe Nevarez <closner_nevarez@hotmail.com>
2015-03-28 13:10:40 -0500

    fixed script in sublime

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,15 @@
+-- From your vagrant environment, run the script with
+--  mysql -u codeup -p < albums_migration.sql 
  USE codeup_test_db;
+
+ DROP TABLE IF EXISTS albums;
+
+ CREATE TABLE albums (
+ 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ 	artist VARCHAR(100) NOT NULL,
+ 	name VARCHAR(100) NOT NULL,
+ 	release_date YEAR(4) NOT NULL,
+ 	sales FLOAT NOT NULL,
+ 	genre VARCHAR(100) DEFAULT 'NONE',
+ 	PRIMARY KEY (id)
+ 	);
