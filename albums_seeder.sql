5e0c3ba1f61bac7cfb4c282cc5730fe3da805bf5
Noe Nevarez <closner_nevarez@hotmail.com>
2015-03-30 10:12:21 -0500

    sql update

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Micheal Jackson', 'Thriller', '1982', '42.4', 'Pop,Rock,R&B'),
+	   ('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'Progressive Rock'),
+	   ('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'R&B'),
+	   ('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard Rock'),
+	   ('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock');
---
+++ select_exercise.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+
+
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT * FROM albums WHERE release_date >= '1990';
